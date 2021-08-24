dcc1d4f6d4125854116ab011eccb5d9ad8d9ed40
Maxim <dasvero@gmail.com>
2021-08-24 14:52:59 +0300

    full adapt added

---
+++ js/menu.js
@@ -0,0 +1,13 @@
+const burger = document.querySelector('.header__burger'),
+      burgerMenu = document.querySelector('.burger__block');
+
+burger.addEventListener('click', () => {
+    if (burger.classList.contains('active')) {
+        burger.classList.remove('active');
+        burgerMenu.classList.remove('active');
+    } else {
+        burger.classList.add('active');
+        burgerMenu.classList.add('active');
+    }
+});
+
