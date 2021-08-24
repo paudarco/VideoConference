285b67ee0f27207c8daad3b9fe452c19033bb211
Maxim <dasvero@gmail.com>
2021-08-24 21:05:48 +0300

    improved features blocks visibility on mobile

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const intro = document.querySelector('.intro'),
       partners = document.querySelector('.partners'),
-      features = document.querySelector('.features');
+      features = document.querySelector('.features'),
+      featuresItems = features.querySelectorAll('.features__block');
 
 function showSection(section) {
     if (window.pageYOffset >= section.offsetTop - 450) {
@@ -23,7 +24,22 @@ window.addEventListener('scroll', function showPartners() {
     removeEvent(partners, showPartners);
 });
 
-window.addEventListener('scroll', function showFeatures() {
+window.addEventListener('scroll', function showFeaturesBlock() {
     showSection(features);
-    removeEvent(features, showFeatures);
+    removeEvent(features, showFeaturesBlock);
+});
+
+window.addEventListener('scroll', function showFeatures1() {
+    showSection(featuresItems[0]);
+    removeEvent(featuresItems[0], showFeatures1);
+});
+
+window.addEventListener('scroll', function showFeatures2() {
+    showSection(featuresItems[1]);
+    removeEvent(featuresItems[1], showFeatures2);
+});
+
+window.addEventListener('scroll', function showFeatures3() {
+    showSection(featuresItems[2]);
+    removeEvent(featuresItems[2], showFeatures3);
 });
