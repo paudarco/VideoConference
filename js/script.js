e94cfb6ff205f40be3ea870c1b1c64b2f49e8ba2
Maxim <dasvero@gmail.com>
2021-08-24 15:37:14 +0300

    full adapt and showing screen

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-const burger = document.querySelector('.header__burger'),
-      burgerMenu = document.querySelector('.burger__block');
+const intro = document.querySelector('.intro'),
+      partners = document.querySelector('.partners'),
+      features = document.querySelector('.features');
 
-burger.addEventListener('click', () => {
-    if (burger.classList.contains('active')) {
-        burger.classList.remove('active');
-        burgerMenu.classList.remove('active');
-    } else {
-        burger.classList.add('active');
-        burgerMenu.classList.add('active');
+function showSection(section) {
+    if (window.pageYOffset >= section.offsetTop - 450) {
+        section.classList.remove('hide');
     }
+}
+
+function removeEvent(section, funcName) {
+    if (window.pageYOffset >= section.offsetTop - 449) {
+        window.removeEventListener('scroll', funcName);
+    }
+}
+
+setTimeout(() => {
+    showSection(intro);
+}, 300);
+
+window.addEventListener('scroll', function showPartners() {
+    showSection(partners);
+    removeEvent(partners, showPartners);
+});
+
+window.addEventListener('scroll', function showFeatures() {
+    showSection(features);
+    removeEvent(features, showFeatures);
 });
