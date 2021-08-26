ed3881aa2f132eab8b04cdabbbf68e8c74b995fb
Maxim <dasvero@gmail.com>
2021-08-26 20:36:57 +0300

    add 2 anchors for menu

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,26 @@
 const burger = document.querySelector('.header__burger'),
-      burgerMenu = document.querySelector('.burger__block');
+      burgerMenu = document.querySelector('.burger__block'),
+      menuItems = burgerMenu.querySelectorAll('.header__menu__item');
+
+function showMenu() {
+    burger.classList.add('active');
+    burgerMenu.classList.add('active');
+}
+
+function hideMenu() {
+    burger.classList.remove('active');
+    burgerMenu.classList.remove('active');
+}
 
 burger.addEventListener('click', () => {
     if (burger.classList.contains('active')) {
-        burger.classList.remove('active');
-        burgerMenu.classList.remove('active');
+        hideMenu();
     } else {
-        burger.classList.add('active');
-        burgerMenu.classList.add('active');
+        showMenu();
     }
+});
+
+menuItems.forEach(item => {
+    item.addEventListener('click', () => hideMenu())
 });
 
